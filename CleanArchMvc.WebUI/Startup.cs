202974dce52d1b2f881abd9faeb5b58cf10e731e
Eduardo Paes <eduardo-paes@outlook.com>
2022-02-18 14:27:26 -0300

    Add project files.

---
+++ CleanArchMvc.WebUI/Startup.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Infra.IoC;
+
+namespace CleanArchMvc.WebUI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastructure(Configuration);
+            services.AddControllersWithViews();
+        }
+    }
+}
